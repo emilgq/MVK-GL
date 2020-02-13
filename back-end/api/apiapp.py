0f4b479b54193e209d2ebb4c1c351093d5a4e9c3
Emil Gunnberg Querat <emil.g.q@fairlo.se>
2020-02-13 21:46:11 +0100

    Implementation of flask_restful and refined placeholder data

--- back-end/api/apiapp.py
+++ back-end/api/apiapp.py
@@ -1,46 +1,90 @@
 from flask import Flask
+from flask_restful import Resource, Api, reqparse, abort
+import json, random
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/placeholder/random-json")
-def hello():
-    return """{ "users": [ { "userId": 1, "firstName": "Krish", "lastName": "Lee", "phoneNumber": "123456", "emailAddress": "krish.lee@learningcontainer.com" }, { "userId": 2, "firstName": "racks", "lastName": "jacson", "phoneNumber": "123456", "emailAddress": "racks.jacson@learningcontainer.com" }, { "userId": 3, "firstName": "denial", "lastName": "roast", "phoneNumber": "33333333", "emailAddress": "denial.roast@learningcontainer.com" }, { "userId": 4, "firstName": "devid", "lastName": "neo", "phoneNumber": "222222222", "emailAddress": "devid.neo@learningcontainer.com" }, { "userId": 5, "firstName": "jone", "lastName": "mac", "phoneNumber": "111111111", "emailAddress": "jone.mac@learningcontainer.com" } ] }"""
+# Arguments required to train a new model.
+createModelParser = reqparse.RequestParser()
+createModelParser.add_argument('modelID', type=str)
+createModelParser.add_argument('model-type', type=str, help='Choose a model type from XGBoost, CatBoost or LightGBM')
+createModelParser.add_argument('learning-rate', type=float, help='Learning rate between 0 and 1')
+createModelParser.add_argument('max-depth', type=int, help='Max Depth')
 
-@app.route("/placeholder/model-result")
-def snd():
-    return """{
-  "modelID": "XGB001",
-  "model-type": "XGBosst",
-  "parameters": {
-    "learning_rate": "0.5",
-    "max_depth": "10"
-  },
-  "results": {
-    "00:00": "1.2",
-    "01:00": "1.4",
-    "02:00": "1.5",
-    "03:00": "1.7",
-    "04:00": "2.0",
-    "05:00": "2.3",
-    "06:00": "2.3",
-    "07:00": "2.4",
-    "08:00": "2.9",
-    "09:00": "3.0",
-    "10:00": "3.5",
-    "11:00": "3.5",
-    "12:00": "3.8",
-    "13:00": "3.9",
-    "14:00": "4.0",
-    "15:00": "4.0",
-    "16:00": "4.2",
-    "17:00": "4.4",
-    "18:00": "4.6",
-    "19:00": "4.7",
-    "20:00": "4.9",
-    "21:00": "5.0",
-    "22:00": "5.1",
-    "23:00": "5.5"
-  }
-}"""
+# Sample post request
+# curl http://localhost:5000/v0/trained-models -d "modelID=CB002" -d "model-type=CatBoost" -d "learning-rate=0.5" -d "max-depth=10" -X POST -v
+
+
+testModelID = "XGB001"
+testModelType = "XGBoost"
+testRMSE = 15000
+testLoadPrediction = [-300000,-290000,-280000,-270000,-290000,-310000,-300000,-310000,-320000,-330000,-340000,-310000,-320000,-290000,-280000,-300000,-310000,-330000,-300000,-280000,-290000,-280000,-270000,-320000]
+testLearningRate = 0.5
+testMaxDepth = 10
+
+MODEL_RESULT = {
+    "modelID": "XGB001",
+    "model-type": "XGBoost",
+    "hyperparameters": {
+        "learning-rate": 0.5,
+        "max-depth": 10
+    },
+    "rmse": 15000,
+    "prediction": [
+        -300000, -290000, -280000, -270000, -290000, -310000, -300000, -310000, -320000, -330000, -340000, -310000, -320000, -290000, -280000, -300000, -310000, -330000, -300000, -280000, -290000, -280000, -270000, -320000
+    ]
+}
+
+# Sample
+TRAINED_MODELS = {
+  'XGB001': {"model-type": "XGBoost", "learning-rate": 0.6, "max-depth": 11, "rmse": 15000},
+  'CB001': {"model-type": "CatBoost", "learning-rate": 0.5, "max-depth": 8, "rmse": 17500},
+  'XGB002': {"model-type": "XGBoost", "learning-rate": 0.5, "max-depth": 8, "rmse": 20000},
+  'XGB003': {"model-type": "XGBoost", "learning-rate": 0.4, "max-depth": 5, "rmse": 12500},
+  'LGBM001': {"model-type": "LightGBM", "learning-rate": 0.5, "max-depth": 8, "rmse": 10000},
+}
+
+class ModelResult(Resource):
+  def get(self, model_id):
+    # Given the model_id, fetch reference info from Database
+    # Pass weather forecast through trained model (.score())
+    response = {
+      "modelID": MODEL_RESULT["modelID"],
+      "model-type": MODEL_RESULT["model-type"],
+      "hyperparameters": MODEL_RESULT["hyperparameters"],
+      "rmse": MODEL_RESULT["rmse"],
+      "prediction": MODEL_RESULT["prediction"]
+      }
+    return response, 200
+
+class TrainedModels(Resource):
+  # List all trained models
+  def get(self):
+    return TRAINED_MODELS
+
+  # Create new Machine Learning Model
+  def post(self):
+    args = createModelParser.parse_args(strict=True)
+    if args['model-type'] not in ['XGBoost', 'CatBoost', 'LightGBM']:
+      abort(404,message='Model type \"{}\" is not provided in this application. Select \"XGBoost\", \"CatBoost\" or \"LightGBM\"'.format(args['model-type']))
+    if ((args['learning-rate'] <= 0) or (args['learning-rate'] >= 1)): 
+      abort(404,message='Learning rate must be between 0 and 1')
+    if args['max-depth'] > 15:
+      abort(404,message='Max depth is capped at 15')
+    if args['modelID'] in TRAINED_MODELS:
+      abort(404,message='Model ID already taken')
+    # Run machine learning module
+    # Fetch reference from database
+    TRAINED_MODELS[args['modelID']] = {
+      "model-type": args['model-type'], 
+      "learning-rate": args['learning-rate'], 
+      "max-depth": args['max-depth'], 
+      "rmse": random.randint(10000,30000)}
+    return TRAINED_MODELS[args['modelID']]
+
+api.add_resource(ModelResult, '/v0/model-result/<model_id>')
+api.add_resource(TrainedModels, '/v0/trained-models')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
