bd5fe1e7063c46c741af2eb3195baec75b4b4235
Mathilda Schwieler <schwi@kth.se>
2020-02-17 17:52:26 +0100

    Queries för att skapa databas

---
+++ back-end/db/MVK_databas.sql
@@ -0,0 +1,7 @@
+CREATE TABLE weather_data
+  (Timestamp DATETIME PRIMARY KEY, Teperature DECIMAL, Overcast DECIMAL, Wind DECIMAL, Consumption DECIMAL);
+
+CREATE TABLE weather_forecast
+  (Timestamp DATETIME PRIMARY KEY, Teperature DECIMAL, Overcast DECIMAL, Wind DECIMAL)
+
+CREATE TABLE 
