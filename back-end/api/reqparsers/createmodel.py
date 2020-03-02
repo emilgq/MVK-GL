2a5fcd1e7d442b67fd225466d74784fb33efb835
Emil Gunnberg Querat <emil.g.q@fairlo.se>
2020-03-02 15:51:59 +0100

    refactored request parsers as separate py files

---
+++ back-end/api/reqparsers/createmodel.py
@@ -0,0 +1,12 @@
+from flask_restful import reqparse
+
+def createmodelparser():
+  cmparser = reqparse.RequestParser()
+  cmparser.add_argument('modelID', type=str)
+  cmparser.add_argument('model-type', type=str, help='Choose a model type from XGBoost, RandomForest or LinearRegression')
+  cmparser.add_argument('learning-rate', type=float, help='Learning rate between 0 and 1')
+  cmparser.add_argument('max-depth', type=int, help='Max Depth')
+  cmparser.add_argument('train-split', type=int, help='Training Split')
+  cmparser.add_argument('validation-split', type=int, help='Validation Split')
+  cmparser.add_argument('API-KEY', type=str, help='Authentication')
+  return cmparser
