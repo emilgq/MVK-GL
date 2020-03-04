8d0597d5416cafeb4aa9637848d5b69b62d3ee1a
Emil Gunnberg Querat <emil.g.q@fairlo.se>
2020-03-04 12:09:13 +0100

    refactored modelID to modelName

--- back-end/api/reqparsers/createmodel.py
+++ back-end/api/reqparsers/createmodel.py
@@ -2,7 +2,7 @@ from flask_restful import reqparse
 
 def createmodelparser():
   cmparser = reqparse.RequestParser()
-  cmparser.add_argument('modelID', type=str)
+  cmparser.add_argument('model-name', type=str)
   cmparser.add_argument('model-type', type=str, help='Choose a model type from XGBoost, RandomForest or LinearRegression')
   cmparser.add_argument('learning-rate', type=float, help='Learning rate between 0 and 1')
   cmparser.add_argument('max-depth', type=int, help='Max Depth')
