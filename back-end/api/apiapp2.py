5650ccc9d3420a96d750db1beee86c488bf6aaf8
Emil Gunnberg Querat <emil.g.q@fairlo.se>
2020-03-10 11:06:32 +0100

    modelresult, weather data and forecast fully implemented, removed obsolete libs

--- back-end/api/apiapp2.py
+++ back-end/api/apiapp2.py
@@ -1,15 +1,9 @@
 import random, re, datetime
 from flask import Flask, json, abort, request, Response
-from flask_restful import reqparse
 import psycopg2
 
 # To enable Cross Origin Requests (such as JS .fetch())
 from flask_cors import CORS
-
-# Import application-specific argument parsers
-from reqparsers.createmodel import createmodelparser
-from reqparsers.updateforecast import updateforecastparser
-from reqparsers.adddata import addDataParser
 
 # Import database conf and parser
 from config import config
@@ -22,19 +16,10 @@ app = Flask(__name__)
 # Enable JavaScript Fetch
 cors = CORS(app, resources={r"/api/v0/*": {"origins": "*"}})
 
-# Arguments parser for training new model.
-createModelParser = createmodelparser()
-
-# Arguments parser for updating weather forecast.
-updateForecastParser = updateforecastparser()
-
-# Arguments parser for adding data
-addDataParser = addDataParser()
-
 # Sample post request
 # curl http://localhost:5000/api/v0/project -d '{"model-name": "XXXXX", "model-type": "RandomForest", "learning-rate": 0.5, "max-depth":10, "train-split": 75, "validation-split": 25, "API-KEY": "MVK123"}' -X POST -v -H "Content-Type: application/json"
-# curl http://localhost:5000/api/v0/weather-forecast -d '{"timestamp":"00:00", "wind": 0, "temperature": 0, "cloud-cover": 0, "API-KEY":"MVK123"}' -X POST -v -H "Content-Type: application/json"
-# curl http://localhost:5000/api/v0/weather-data -d '{"timestamp":"1999-02-02 11:11", "day": 1, "hour":1, "month":2, "temperature":280, "cloud-cover":99, "wind":14, "consumption":10, "API-KEY":"MVK123"}' -X POST -v -H "Content-Type: application/json"
+# curl http://localhost:5000/api/v0/weather-forecast -d '{"timestamp":"2021-02-02 11:00", "wind": 0, "temperature": 0, "cloud-cover": 0, "API-KEY":"MVK123"}' -X POST -v -H "Content-Type: application/json"
+# curl http://localhost:5000/api/v0/weather-data -d '{"timestamp":"1999-02-02 11:11", "temperature":280, "cloud-cover":99, "wind":14, "consumption":10, "API-KEY":"MVK123"}' -X POST -v -H "Content-Type: application/json"
 
 # Sample data
 APIKEY = "MVK123"
@@ -54,42 +39,47 @@ def runDBQuery(query, parameters):
     try:
       query_result = cur.fetchall()
       query_columns = [desc[0] for desc in cur.description]
-    except:
-      pass
+    except Exception as error:
+      raise Exception(error)
     finally:
       conn.commit()
       cur.close()
-
-  except (Exception, psycopg2.DatabaseError) as error:
-    print(error)
-    raise Exception(error)
-
-  finally:
-    if conn is not None:
-      conn.close()
       if query_result is None: 
         return
       else: 
         return query_result, query_columns
 
+  except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+    if conn is not None:
+      conn.close()
+    raise Exception(error)
 
 # API Resource for fetching model specific results
 @app.route('/api/v0/project/<model_id>', methods=['GET'])
 def modelresult(model_id):
   if request.method == 'GET':
     # Given the model_id, fetch reference info from Database
-    # Pass weather forecast through trained model (.score())
-    if model_id not in TRAINED_MODELS:
+    query = "select * from ml_models where model_id = %s"
+    try:
+      result, cols = runDBQuery(query, (model_id,))
+    except Exception as e:
+      abort(Response('Error: {}'.format(e), 400))
+
+    if len(result) == 0:
       abort(Response('Model not found', 404))
-    response = TRAINED_MODELS[model_id].copy()
+
+    # Pass weather forecast through trained model (.score())
+    response = dict(zip(cols, result[0]))
+    # Add result column
     response['result'] = dict(zip(testTimes, testLoadPrediction))
-    return response, 200
+    return json.dumps(response), 200
 
 # API Resource for fetching information about trained models and creating new instances
 @app.route('/api/v0/project', methods=['GET', 'POST'])
 def project():
   if request.method == 'GET':
-    query = "select model_id, model_name, time_creation::varchar, configurations, owner, status, rmse from ml_models"
+    query = "select * from ml_models"
     parameters = None
     result, columns = runDBQuery(query, parameters)
     response = {}
@@ -99,8 +89,8 @@ def project():
       response[result[i][0]] = {}
       for j in range (1, len(result[i])):
         response[result[i][0]][columns[j]] = result[i][j]
+    return json.dumps(response), 200
 
-    return response, 200
   if request.method == 'POST':
   # Create new Machine Learning Model
     args = request.get_json()
@@ -140,26 +130,7 @@ def project():
 @app.route('/api/v0/weather-forecast', methods=['GET', 'POST'])
 def weatherForecast():
   if request.method == 'GET':
-    return WEATHER_FORECAST
-
-  if request.method == 'POST':
-    args = request.get_json()
-    if not re.match(r"([0-1]?[0-9]|2[0-3]):00", args['timestamp']):
-      abort(404,message='Timestamp not right format')
-    if args['API-KEY'] != APIKEY:
-      abort(404, message='unauthorized')
-    WEATHER_FORECAST[args['timestamp']] = {
-      "wind": args["wind"],
-      "temperature": args["temperature"],
-      "cloud-cover": args["cloud-cover"]
-    }
-    return WEATHER_FORECAST[args['timestamp']]
-
-# API Resource for fetching the weather data and adding it to the database
-@app.route('/api/v0/weather-data', methods=['GET', 'POST'])
-def weatherData():
-  if request.method == 'GET':
-    query = "select ts, day, hour, month, temperature, cloud_cover, wind, consumption from weather_data"
+    query = "select * from weather_forecast"
     parameters = None
     result, _ = runDBQuery(query, parameters)
     response = []
@@ -177,7 +148,62 @@ def weatherData():
     args = request.get_json()
     # Validate given arguments
     # Check timestamp format YYYY-MM-DD HH:00
-    if not re.match(r"\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) (0[0-9]|1[0-9]|2[0-3]):00", args['ts']):
+    if not re.match(r"\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) (0[0-9]|1[0-9]|2[0-3]):00", args['timestamp']):
+      abort('Timestamp not right format', 400)
+    # Check decimal values
+    try:
+      temp = float(args['temperature'])
+      wind = float(args['wind'])
+      cc = float(args['cloud-cover'])
+    except:
+      abort(Response(json.dumps({"message": "Type Error of weather metrics or load"}), 400))
+    
+    ts = datetime.datetime.strptime(args['timestamp'], '%Y-%m-%d %H:%M')
+
+    # Extract datetime specifics
+    dow = datetime.datetime.weekday(ts)
+    hod = ts.hour
+    moy = ts.month
+
+    # Attempt to delete current tuple with given hour of day
+    query = "delete from weather_forecast where hour = (%s)"
+    try:
+      runDBQuery(query,(hod,))
+    except:
+      pass
+
+    # Run insert query to weather_forecast
+    query = "insert into weather_forecast (ts, day, hour, month, temperature, cloud_cover, wind) values (%s,%s,%s,%s,%s,%s,%s)"
+    parameters = (ts, dow, hod, moy, temp, cc, wind,)
+    try:
+      runDBQuery(query, parameters)
+      return Response(json.dumps({"message": "Data point successfully added", "Data point": parameters}), 200)   
+    except Exception as e:
+      abort(Response('Error: {}'.format(e), 400))
+
+# API Resource for fetching the weather data and adding it to the database
+@app.route('/api/v0/weather-data', methods=['GET', 'POST'])
+def weatherData():
+  if request.method == 'GET':
+    query = "select * from weather_data"
+    parameters = None
+    result, _ = runDBQuery(query, parameters)
+    response = []
+    # Convert list of list to dict of dict with columns as keys
+    for i in range(0, len(result)):
+      response.append([])
+      for j in range (0, len(result[i])):
+        if j == 0:
+          response[i].append(result[i][j].isoformat())
+        else:
+          response[i].append(result[i][j])
+    return json.dumps(response), 200
+
+  if request.method == 'POST':
+    args = request.get_json()
+    # Validate given arguments
+    # Check timestamp format YYYY-MM-DD HH:00
+    if not re.match(r"\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) (0[0-9]|1[0-9]|2[0-3]):00", args['timestamp']):
       abort('Timestamp not right format', 400)
     # Check decimal values
     try:
@@ -186,8 +212,9 @@ def weatherData():
       cc = float(args['cloud-cover'])
       load = float(args['consumption'])
     except:
-      abort(Response('Type Error of weather metrics or load'), 400)
-    ts = datetime.datetime.strptime(args['ts'], '%Y-%m-%d %H:%M')
+      abort(Response(json.dumps({"message": "Type Error of weather metrics or load"}), 400))
+    
+    ts = datetime.datetime.strptime(args['timestamp'], '%Y-%m-%d %H:%M')
 
     # Extract datetime specifics
     dow = datetime.datetime.weekday(ts)
@@ -199,43 +226,9 @@ def weatherData():
     # Run insert query to weather_data
     try:
       runDBQuery(query, parameters)
-      message = 'data point successfully added'
-      return message, 200
+      return Response(json.dumps({"message": "Data point successfully added", "Data point": parameters}), 200)   
     except Exception as e:
       abort(Response('Error: {}'.format(e), 400))
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-""" 
-In order to integrate the app with the database we'll use the psycopg2 library. 
-
-It provides a cursor class which serves as an interface to the database. 
-
-We'll need a function which takes a parametrized SQL statement, e.g. "SELECT * FROM weather_forecast WHERE timestamp < %s"
-together with the parameters for the desired query and executes the combined statement.
-
-The following procedure should take place(see egq/lms-dbas/ui for reference):
-1. Setup a connection element
-
-Within a try except clause, 
-2. Load configuration parameters of database
-3. Connect and create psycopg2 cursor.
-4. Construct and execute query
-5. Fetch and store result
-6. Commit any potential changes of the transaction
-7. Close cursor
-
-If exception
-8. Throw database error
-
-Finally
-9. Close connection
-
-The return value should either be the resulting table of the query or none, depending on 
-the nature of the query (SELECT/INSERT/DELETE). The cursor returns a list of tuples.
-
-The intended usage of the function is that an endpoint request should trigger a database query. 
-The endpoint function has the query structure stored as a local variable. The query parameters are given either by 
-request header, data or url-parameters (e.g. model_id). 
-"""
