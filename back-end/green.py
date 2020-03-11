eb7eda7a18ad75d1e843e5a5271358e7d80743df
Eric Fredin Haslum <ehaslum@kth.se>
2020-03-11 13:11:45 +0100

    merged

--- back-end/green.py
+++ back-end/green.py
@@ -3,10 +3,13 @@ import requests
 import re
 import pandas as pd
 import json
+from datetime import datetime
+import dateutil.parser
 
-# Stockholms kordinater på en höjd av 59 ger cloud coverage
+
+# Stockholms kordinater på en höjd av 59
 endpoint_url = "https://api.greenlytics.io/weather/v1/get_nwp"
-headers = {"Authorization": "1iqsmV9rE6UhCkyzosBpROkGVgv0BrQ87aCPqLtV4VrBPwf0HbSESt8twLuDj3lrKUmj9sSe"}
+headers = {"Authorization": ""}
 params = {
     'model': 'DWD_ICON-EU',
     'start_date': '2019-05-15 00',
@@ -32,23 +35,59 @@ df_load.index = pd.to_datetime(df_load['Datetime'])
 #df_load = df_load.drop(columns='Datetime')
 df_load['Load'] = -df_load['Load']/10**3
 
-
 # greenlytics
 response = requests.get(endpoint_url, headers=headers, params={'query_params': json.dumps(params)})
 df_green = pd.read_json(response.text)
 
-#print(df)
+
+#print
+dategl = dateutil.parser.parse(df_green['valid_datetime'][1])
+#dateygl = datetime.strptime((df_green['valid_datetime'][1]), "%Y-%m-%d %H:%M")
+print(type(dategl))
+
+# Adds column with as a datetime object where timezone is removed. Makes it possible to compare dataframes
+df_green['date_valid_datetime'] = [(dateutil.parser.parse(row)).replace(tzinfo=None) for row in df_green['valid_datetime']]
+
+# Adds column with date as a datetime object. Makes it possible to compare dataframes
+df_load['date_Datetime'] = [(dateutil.parser.parse(rows)) for rows in df_load['Datetime']]
+
+#print(df_green)
+#print(df_load)
+
+mergedSets = pd.merge(
+    df_load,
+    df_green,
+    left_on=['date_Datetime'],
+    right_on=['date_valid_datetime']
+)
+
+print(mergedSets)
+#MAKES datetime from str to datetime, used to get weekday of%H:%M"
+#datey = datetime.strptime((df_load['Datetime'][1]), "%Y-%m-%d%t%H:%M")
+# print(datey)
+
+#df["time_column_name"] = df["time_column_name"].apply(lambda x: datetime.datetime.strptime(x , "datetime_format"))
+
+
 # print("Tempeture at heigt 59 is %s and CloudCoverage is %s" % (df['T_height_59'],df['CLCT']))
-for i in range(len(df_load['Load'])-1):
+#print(type(df_load['Datetime'][1]))
+for i in range(len(mergedSets)):
+    print(mergedSets['date_Datetime'])
     # +1 för att få så att green och svks timmar stämmer överense
-    dateAndTime = re.split(r"T",df_green['valid_datetime'][i+1])
     #,df['V'][i]
     #psqlString(df['valid_datetime'][i])
     # DateTime uppdelad
 
+
     #psqlString = "INSERT INTO greenlytics (date,time,T_height_,CLCT,Wind) values (%s, %s, %s, %s, %s) " % (str(dateAndTime[0]),str(dateAndTime[1]),df_green['T_height_59'][i],df_green['CLCT'][i],df_green['V_height_59'][i])
 
-    psqlString = "INSERT INTO greenlytics (date,time,T_height_,CLCT,Wind) values (%s, %s, %s, %s) " % (str(dateAndTime[0]),str(dateAndTime[1]),df_load['Load'][i],df_load['Datetime'][i])
+    #psqlString = datetime.strptime((df_load['Datetime'][1]), "%Y-%m-%d %H:%M")
+    #print(psqlString)
+    # Använd något i denna stil
+    # --- psqlString = "INSERT INTO greenlytics (date,time,T_height_,CLCT,Wind) values (%s) " % (type(df_load['Datetime'][i]))
+
+    #ANVÄND
+    #psqlString = "INSERT INTO greenlytics (date,time,T_height_,CLCT,Wind) values (%s, %s, %s, %s) " % (str(dateAndTime[0]),str(dateAndTime[1]),df_load['Load'][i],df_load['Datetime'][i])
     #psqlString = "INSERT INTO greenlytics (date,time,T_height_,CLCT,Wind) values (%s, %s, %s, %s, %s) " % (str(dateAndTime[0]),str(dateAndTime[1]),df_green['T_height_59'][i],df_green['CLCT'][i],df_green['V_height_59'][i])
     #psqlString = "INSERT INTO greenlytics (datetime,T_height_,CLCT,Wind) values ( %s, %s, %s, %s) " % (df['valid_datetime'][i],df['T_height_59'][i],df['CLCT'][i],df['V_height_59'][i])
-    print(psqlString)
+    # --- print(psqlString)
