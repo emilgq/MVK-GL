ebdd161ecfc287adfd2caf83a5345725154b648a
Eric Fredin Haslum <ehaslum@kth.se>
2020-04-08 13:34:09 +0200

    drops duplicates before merge

--- back-end/green.py
+++ back-end/green.py
@@ -12,8 +12,8 @@ endpoint_url = "https://api.greenlytics.io/weather/v1/get_nwp"
 headers = {"Authorization": "1iqsmV9rE6UhCkyzosBpROkGVgv0BrQ87aCPqLtV4VrBPwf0HbSESt8twLuDj3lrKUmj9sSe"}
 params = {
     'model': 'DWD_ICON-EU',
-    'start_date': '2019-12-20  00',
-    'end_date': '2020-03-01 00',
+    'start_date': '2019-08-16  00',
+    'end_date': '2019-08-17 00',
     'coords': {'latitude': [59], 'longitude': [18], 'height': [59]},
     'variables': ['T', 'CLCT','V'],
     'as_dataframe': True
@@ -26,7 +26,7 @@ df_green = pd.read_json(response.text)
 # SVK API
 # Energy Load i MKWh for Stockholm area.
 date_start = '2019-08-16'
-date_end = '2020-03-01'
+date_end = '2019-08-17'
 area = 'STH'
 url_base = 'https://mimer.svk.se/'
 url_target = 'ConsumptionProfile/DownloadText?groupByType=0&' + \
@@ -45,8 +45,13 @@ df_load['Load'] = -df_load['Load']/10**3
 #dategl = dateutil.parser.parse(df_green['valid_datetime'][1])
 #dateygl = datetime.strptime((df_green['valid_datetime'][1]), "%Y-%m-%d %H:%M")
 
+#Drop duplicates with same date in df_green
+df_green_unique = df_green.drop_duplicates(["valid_datetime"])
+
+
 # Adds column with as a datetime object where timezone is removed. Makes it possible to compare dataframes
-df_green['date_valid_datetime'] = [(dateutil.parser.parse(row)).replace(tzinfo=None) for row in df_green['valid_datetime']]
+df_green_unique['date_valid_datetime'] = [(dateutil.parser.parse(row)).replace(tzinfo=None) for row in df_green_unique['valid_datetime']]
+
 
 # Adds column with date as a datetime object. Makes it possible to compare dataframes
 df_load['date_Datetime'] = [(dateutil.parser.parse(rows)) for rows in df_load['Datetime']]
@@ -55,23 +60,25 @@ df_load['date_Datetime'] = [(dateutil.parser.parse(rows)) for rows in df_load['D
 # Merges on the datetime type where they're equal. Keeps both of the columns.
 mergedSets = pd.merge(
     df_load,
-    df_green,
+    df_green_unique,
     left_on=['date_Datetime'],
     right_on=['date_valid_datetime']
 )
 
 mergedSets.dropna()
+print(mergedSets)
 # Posts the merged dataframe to rest-API
-for i in range(len(mergedSets)):
-    endpoint_url = "http://35.228.239.24/api/v1/weather-data"
-    headers = {"Content-Type" : "application/json"}
-    params = {
-    "timestamp": mergedSets["Datetime"][i],
-    "temperature": mergedSets["T_height_59"][i],
-    "cloud-cover": mergedSets["CLCT"][i],
-    "wind": mergedSets["V_height_59"][i],
-    "consumption": mergedSets["Load"][i],
-    "API-KEY": "MVK123"
-    }
-    response = requests.post(endpoint_url, headers=headers, data=json.dumps(params))
-    print(response.text)
+#
+#for i in range(len(mergedSets)):
+#    endpoint_url = "http://35.228.239.24/api/v1/weather-data"
+#    headers = {"Content-Type" : "application/json"}
+#    params = {
+#    "timestamp": mergedSets["Datetime"][i],
+#    "temperature": mergedSets["T_height_59"][i],
+#    "cloud-cover": mergedSets["CLCT"][i],
+#    "wind": mergedSets["V_height_59"][i],
+#    "consumption": mergedSets["Load"][i],
+#    "API-KEY": "MVK123"
+#    }
+#    response = requests.post(endpoint_url, headers=headers, data=json.dumps(params))
+#    print(response.text)
