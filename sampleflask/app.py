ed6ca0deebb0d0ffc78cbb875c98763fc42be235
jag <jag@me.se>
2020-04-16 18:11:18 +0200

    fixed training of models and created required HTML for SVR. Sorry Emil about this commit message accidentally added two files at once

--- sampleflask/app.py
+++ sampleflask/app.py
@@ -5,6 +5,7 @@ import requests, json
 app = Flask(__name__)
 
 app.secret_key = 'MVK123'
+# session['logged_in'] = False
 
 # Authorized wrapper function
 def is_logged_in(f):
@@ -13,7 +14,7 @@ def is_logged_in(f):
     if session['logged_in']:
       return f(*args, **kwargs)
     else:
-      flash("Please enter your credentials in order to access this feature.", "danger")
+      flash("Unauthorized. Please enter you credentials to access this feature.", "danger")
       return redirect(url_for("login"))
   return wrap
 
@@ -21,8 +22,8 @@ def is_logged_in(f):
 # https://flask.palletsprojects.com/en/1.1.x/quickstart/#rendering-templates
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-  if session['logged_in']:
-    return redirect(url_for("project"))
+  # if session['logged_in'] == False:
+  #     return redirect(url_for("login"))
   if request.method == 'GET':
     return render_template('login.html')
   if request.method == 'POST':
@@ -32,19 +33,21 @@ def login():
       flash("Access authorized, welcome :)", "success")
       return redirect(url_for("project"))
     else:
-      error = "Unauthorized"
+      error = "Wrong Authorization key. Please contact the MVK-team"
       return render_template('login.html', error=error)
 
 @app.route('/logout')
+# @is_logged_in
 def logout():
     if session['logged_in']:
       session['logged_in'] = False
       flash("You have been logged out.", "success")
       return redirect(url_for("login"))
     else:
-      session['logged_in'] = False
-      error = "You can't log out if you have yet to log in, silly"
+      # session['logged_in'] = False
+      error = "Quit messing around with the URL, use the buttons like normal people"
       return render_template('login.html', error=error)
+      return redirect(url_for("login"))
 
 
 @app.route('/project')
@@ -67,16 +70,77 @@ def train():
     endpoint_url = "http://35.228.239.24/api/v1/project"
     headers = {"Content-Type" : "application/json"}
 
-    params = {
-      "model-name": request.form['modelname'],
-      "model-type": request.form['modeltype'],
-      "learning-rate": float(request.form['learningrate']),
-      "max-depth": int(request.form['maxdepth']),
-      "train-split": int(request.form['trainsplit']),
-      "validation-split": int(request.form['valsplit']),
-      "API-KEY": "MVK123"
-    }
+    modeltype = request.form['modeltype']
+    print("the model type is " + modeltype)
+    # print (modeltype)
+    if modeltype == "XGBoost":
+        params = {
+        "model-name": request.form['modelname'],
+        "configurations":{
+        "model-type": request.form['modeltype'],
+        "learning-rate": float(request.form['learningrateXG'])/10, # divided by 10 för den gör inte divisionen "i tid" i fetchstadie.
+        "max-depth": int(request.form['maxdepthXG']),
+        "train-split": int(request.form['trainsplitXG'])/100,
+        "validation-split": (100-int(request.form['trainsplitXG']))/100,},
+        "API-KEY": "MVK123"
+        }
+        # print(params)
+        print("The If case: XGBoost happened")
+    elif modeltype == "LinearRegression":
+        params = {
+        "model-name": request.form['modelname'],
+        "configurations":{
+        "model-type": request.form['modeltype'],
+        "learning-rate": float(request.form['learningrateLR']),
+        "train-split": int(request.form['trainsplitLR']),
+        "validation-split": int(request.form['valsplitLR']),
+        "max-depth": int(request.form['maxdepthLR'])},
+        "API-KEY": "MVK123"
+        }
+        # print(params)
+        print("The If case: Linear Regression happened")
+    elif modeltype == "RandomForest":
+        params = {
+        "model-name": request.form['modelname'],
+        "configurations":{
+        "model-type": request.form['modeltype'],
+        "n-estimators": float(request.form['n-estimatorRF']),
+        "max-depth": int(request.form['maxdepthRF']),
+        "train-split": int(request.form['trainsplitRF'])/100,
+        "validation-split": (100-int(request.form['trainsplitRF']))/100,},
+        "API-KEY": "MVK123"
+        }
+        # print("watafak")
+        print(params)
+        print("The If case: Random Forest happened")
+    elif modeltype == "SVR":
+        params = {
+        "model-name": request.form['modelname'],
+        "configurations":{
+        "model-type": request.form['modeltype'],
+        "kernel": request.form['SVR KERNEL'],
+        "C": int(request.form['CSVR']),
+        "train-split": int(request.form['trainsplitSVR'])/100,
+        "validation-split": (100-int(request.form['trainsplitSVR']))/100,},
+        "API-KEY": "MVK123"
+        }
+        print("The If case: SVR happened")
+    else:
+        print("The else case happened")
+        flash("Error, something went wrong")
+        return redirect(url_for("train"))
+
+    # params = {
+    #   "model-name": request.form['modelname'],
+    #   "model-type": request.form['modeltype'],
+    #   "learning-rate": float(request.form['learningrate']),
+    #   "max-depth": int(request.form['maxdepth']),
+    #   "train-split": int(request.form['trainsplit']),
+    #   "validation-split": int(request.form['valsplit']),
+    #   "API-KEY": "MVK123"
+    # }
     print(params)
+    print("Hello this should get a response")
     response = requests.post(endpoint_url, headers=headers, data=json.dumps(params))
     return response.text
 
